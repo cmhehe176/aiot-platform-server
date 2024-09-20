d5a2f9831676e6bc8f9584f833a84d6df9352d84
Cong Minh <ndmc.system176@gmail.com>
2024-09-20 21:17:17 +0700

    fix: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = await app.get(ConfigService);
-
+  app.enableCors();
   await app.listen(config.get<string>('PORT'));
 }
 
