cb52fcd742bce75cc4622a2bdb6414a8a3122a73
Cong Minh <ndmc.system176@gmail.comi>
2024-10-14 13:04:31 +0700

    feat: add source mail for reply support

--- src/feature/email/email.source.ts
+++ src/feature/email/email.source.ts
@@ -42,12 +42,38 @@ export const SourceMailSupport = `
         
         <div style="margin-top: 10px;">Thank you for using our services. We have received your request for support and truly appreciate your feedback on this matter. We will get back to you as soon as possible.</div>
         
-        <div style="margin-top: 10px;">Please note that my name is {{name}}, will be reviewing your request and responding shortly.</div>
+        <div style="margin-top: 10px;">Please note that we will be reviewing your request and responding shortly.</div>
         
         <div style="margin-top: 10px;">If you have any further questions, feel free to contact us in our website.</div>
         
         <div style="margin-top: 20px;">Thank you for your patience!</div>
-{{link}}
+
+        <div style="margin-top: 20px; border-top: 1px solid #ccc; padding-top: 10px;">
+            <div>--------------------------------------</div>
+            <div style="font-weight: bold;">${footerMail.title}</div>
+            <div>TEL: ${footerMail.telephone} </div>
+            <div>URL: <a href="${footerMail.url}" style="color: #007BFF; text-decoration: none;">${footerMail.iurl}</a></div>
+            <div>--------------------------------------</div>
+        </div>
+    </div>
+</div>
+`;
+
+export const SourceMailSupportReply = `
+<div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; padding: 20px; background-color: #f4f4f4;">
+    <div style="background: white; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+        <div style="font-size: 24px; font-weight: bold; margin-bottom: 20px;">Thank You for Your Support Request</div>
+        
+        <div>Dear {{name}},</div>
+        
+        <div style="margin-top: 10px;">Thank you for using our services. We have received your request for support and truly appreciate your feedback on this matter. We will get back to you as soon as possible.</div>
+        
+        <div style="margin-top: 10px;">Please note that we will be reviewing your request and responding shortly.</div>
+        
+        <div style="margin-top: 10px;">If you have any further questions, feel free to contact us in our website.</div>
+        
+        <div style="margin-top: 20px;">Thank you for your patience!</div>
+
         <div style="margin-top: 20px; border-top: 1px solid #ccc; padding-top: 10px;">
             <div>--------------------------------------</div>
             <div style="font-weight: bold;">${footerMail.title}</div>
